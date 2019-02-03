9eb297bb0b47fa9a6fd23ec0ef15e63eb1416945
Ben Wenger <ben.wenger@comcast.net>
2019-02-02 23:33:25 -0800

    Fix issue with windows paths

--- src/windows/winheader.h
+++ src/windows/winheader.h
@@ -32,8 +32,14 @@ namespace dshfs
             return wide;
         }
 
-        inline std::wstring makeWindowsFullPathName(const std::string& fn)
+        inline std::wstring makeWindowsFullPathName(std::string fn)
         {
+            for(auto& i : fn)
+            {
+                if(i == '\\')
+                    i = '/';
+            }
+
             Filename fnobj(fn);
             if(!fnobj.fullResolve())
                 throw Err::NotFound("Too many '..'s in directory.  Could not resolve");
